fe6e27d8f22cbe4a1268ad4bb1b992e3326f1b2c
Yuriy Koziy <yukoziy@gmail.com>
2022-07-08 16:02:10 -0400

    method outlines in session class

--- src/main/java/com/revanate/session/Session.java
+++ src/main/java/com/revanate/session/Session.java
@@ -2,6 +2,10 @@ package com.revanate.session;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+
+import com.revanate.transaction.Transaction;
+
 // each session is a connection to the database
 public class Session {
 	
@@ -13,15 +17,52 @@ public class Session {
 		this.conn = conn;
 	}
 	public Object get(Class<?> entityClass, int id) {
+		// ID can be any type, figure out  best way to do it
+		String tableName = entityClass.getName();
+		/* 
+		 * 1) Get the table name (Entity annotation is empty string, use class name as table name as lower case), or specified in Entity annotation tableName
+		 * 2) Get the name of primary key field(s)
+		 * 3) Create query based on that data
+		 * 4) Build an Object based on the result of query?
+		 * 5) If everything went OK, just store the Object inside the cache
+		 * 6) Return primary key (ID), it is set to Object because it can be any type
+		 */	
 		return null;
 	}
 	
-	public Object save(Object object) {
+	public Object save(Object object) {		
+		/* 
+		 * 1) Convert object to EntityModel
+		 * 2) Get field names and field data
+		 * 3) Create query using data from previous data
+		 * 4) Run that query on the DB, using session connection
+		 * 5) If everything went OK, just store the Object inside the cache
+		 * 6) Return primary key (ID), it is set to Object because it can be any type
+		 */
 		return null;
 	}
 	
 	public void delete(Object object) {
-		
+		/* 
+		 * 1) Convert object to EntityModel
+		 * 2) Get fprimaryKey ID
+		 * 3) Create query using data from previous data
+		 * 4) Run that query on the DB, using session connection
+		 * 5) If everything went OK, just store the Object inside the cache
+		 * 6) Return primary key (ID), it is set to Object because it can be any type
+		 */	
+	}
+	
+	public Transaction beginTransaction() {
+		/* 
+		 * 1) Convert object to EntityModel
+		 * 2) Get fprimaryKey ID
+		 * 3) Create query using data from previous data
+		 * 4) Run that query on the DB, using session connection
+		 * 5) If everything went OK, just store the Object inside the cache
+		 * 6) Return primary key (ID), it is set to Object because it can be any type
+		 */	
+		return null;
 	}
 	
 	// clean up and release JDBC connection
