97cff3deb3f33ed191fa8b568efd9b89c532bcb5
okraus22 <odk22@yahoo.com>
2022-07-11 17:08:45 -0400

    started work on entity class

--- src/main/java/com/revanate/entity/EntityModel.java
+++ src/main/java/com/revanate/entity/EntityModel.java
@@ -1,10 +1,76 @@
 package com.revanate.entity;
 
-public class EntityModel<T> {
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Iterator;
 
-	//this would convert normal class to EntityModel class
-	public static EntityModel<Class<?>> of(Class<?> annotetedClass) {
-		// TODO Auto-generated method stub
-		return null;
+import com.revanate.annotations.Column;
+import com.revanate.annotations.Entity;
+import com.revanate.annotations.ForeignKey;
+import com.revanate.annotations.Id;
+
+public class EntityModel {
+
+	
+	private Class<?> annotetedClass;
+	private Field[] fields;
+	private int numColumns = 0, numForeignKeys = 0;
+	private boolean primaryKey = false;
+	
+	
+	public EntityModel (Class<?> annotetedClass)
+	{
+		this.annotetedClass = annotetedClass;
+		isAnnotated();
+		
 	}
+	
+	private void isAnnotated()
+	{
+		if(annotetedClass.isAnnotationPresent(Entity.class))
+		{
+			fields = annotetedClass.getDeclaredFields();
+			
+			for(Field field : fields)
+			{
+				Annotation[] annotations = field.getAnnotations();
+				for(Annotation ann : annotations)
+				{
+					if(ann.annotationType() == Column.class)
+					{
+						numColumns++;
+						break;
+					}
+					
+					else if(ann.annotationType() == ForeignKey.class)
+					{
+						numForeignKeys++;
+						numColumns++;
+						break;
+					}
+					else if (ann.annotationType() == Id.class)
+					{
+						if(!primaryKey)
+						{
+							primaryKey = true;
+							numColumns++;
+							break;
+						}
+						else 
+						{
+							throw new RuntimeException("The id annotation is used for primary keys, and you can not have multiple primary keys");
+						}
+					}
+				}
+			}
+			
+		}
+		else 
+		{
+			throw new RuntimeException("The class was not properly annotated");
+		}
+	}
+	
+	
 }
