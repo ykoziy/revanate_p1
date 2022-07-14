f2023ca7d6b8f23d839d760df6b39bd632817ffc
Yuriy Koziy <yukoziy@gmail.com>
2022-07-14 00:10:54 -0400

    fixed ForeignKey annotation

--- src/main/java/com/revanate/annotations/ForeignKey.java
+++ src/main/java/com/revanate/annotations/ForeignKey.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface ForeignKey
 {
-	public String References();
+	public String columnName();
 }
 
--- src/main/java/com/revanate/entity/ForeignKeyField.java
+++ src/main/java/com/revanate/entity/ForeignKeyField.java
@@ -35,6 +35,6 @@ public class ForeignKeyField
 	}
 
 	public String getColumnName() {
-		return field.getAnnotation(ForeignKey.class).References();
+		return field.getAnnotation(ForeignKey.class).columnName();
 	}	
 }
