41bc13a40519606758c12b7cbe88907504c32044
Yuriy Koziy <yukoziy@gmail.com>
2022-07-14 12:34:06 -0400

    Session is now using SessionCache

--- src/main/java/com/revanate/session/Session.java
+++ src/main/java/com/revanate/session/Session.java
@@ -1,119 +1,148 @@
 package com.revanate.session;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 import com.revanate.query.Query;
 import com.revanate.query.Table;
+import com.revanate.annotations.Id;
 import com.revanate.entity.EntityModel;
 import com.revanate.transaction.Transaction;
 
 // each session is a connection to the database
 public class Session {
 
-	private Connection conn;
+    private Connection conn;
 
-	private SessionCache cache;
+    private SessionCache cache;
 
-	private Transaction currenTransaction;
+    private Transaction currenTransaction;
 
-	private List<EntityModel<Class<?>>> entityModelList;
+    private List<EntityModel<Class<?>>> entityModelList;
 
-	public Session(Connection conn, List<EntityModel<Class<?>>> entityModelList) {
-		this.entityModelList = entityModelList;
-		this.conn = conn;
-		createTables();
-	}
+    public Session(Connection conn, List<EntityModel<Class<?>>> entityModelList) {
+        this.entityModelList = entityModelList;
+        this.conn = conn;
+        cache = new SessionCache();
+        createTables();
+    }
 
-	private void createTables() {
-		Table t = new Table(conn, entityModelList);
-		for (EntityModel<?> entity : entityModelList) {
-			t.createTable(entity);
-		}
-	}
+    private void createTables() {
+        Table t = new Table(conn, entityModelList);
+        for (EntityModel<?> entity : entityModelList) {
+            t.createTable(entity);
+        }
+    }
 
-	public List<EntityModel<Class<?>>> getEntityList() {
-		return entityModelList;
-	}
+    public List<EntityModel<Class<?>>> getEntityList() {
+        return entityModelList;
+    }
 
-	public <T> Object get(Class<?> entityClass, T id) {
-		for (EntityModel<?> entity : entityModelList) {
-			if (entity.getClassName().equals(entityClass.getCanonicalName())) {
-				Query query = new Query(conn, entity);
-				try {
-					return query.get(entityClass, id);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return null;
-	}
+    public <T> Object get(Class<?> entityClass, T id) {
+        for (EntityModel<?> entity : entityModelList) {
+            if (entity.getClassName().equals(entityClass.getCanonicalName())) {
+                Query query = new Query(conn, entity);
+                try {
+                    Object resultObject = query.get(entityClass, id);
+                    if (cache.contains(id.toString())) {
+                        return cache.get(id.toString());
+                    } else {
+                        cache.add(id.toString(), resultObject);
+                    }
+                    return resultObject;
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
 
-	public void update(Object object) {
-		Class<?> clazz = object.getClass();
-		for (EntityModel<?> entity : entityModelList) {
-			if (entity.getClassName().equals(clazz.getCanonicalName())) {
-				Query query = new Query(conn, entity);
-				query.update(object);
-			}
-		}
-	}
+    public void update(Object object) {
+        Class<?> clazz = object.getClass();
+        for (EntityModel<?> entity : entityModelList) {
+            if (entity.getClassName().equals(clazz.getCanonicalName())) {
+                Query query = new Query(conn, entity);
+                query.update(object);
+            }
+        }
+    }
 
-	public Object save(Object object) {
-		Class<?> clazz = object.getClass();
-		for (EntityModel<?> entity : entityModelList) {
-			if (entity.getClassName().equals(clazz.getCanonicalName())) {
-				Query query = new Query(conn, entity);
-				return query.save(object);
-			}
-		}
-		return null;
-	}
+    public Object save(Object object) {
+        Class<?> clazz = object.getClass();
+        for (EntityModel<?> entity : entityModelList) {
+            if (entity.getClassName().equals(clazz.getCanonicalName())) {
+                Query query = new Query(conn, entity);
+                Object resultObject = query.save(object);
+                if (resultObject != null) {
+                    cache.add(resultObject.toString(), object);
+                }
 
-	public void delete(Object object) {
-		for (EntityModel<?> entity : entityModelList) {
-			if (entity.getClassName().equals(object.getClass().getCanonicalName())) {
-				Query query = new Query(conn, entity);
-				try {
-					query.delete(object);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+                return resultObject;
+            }
+        }
+        return null;
+    }
 
-	public Query getAll(Class<?> resultType) {
-		for (EntityModel<?> entity : entityModelList) {
-			if (entity.getClassName().equals(resultType.getName())) {
-				Query query = new Query(conn, entity);
-				try {
-					return query.getAll(resultType);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return null;
-	}
+    public void delete(Object object) {
+        for (EntityModel<?> entity : entityModelList) {
+            if (entity.getClassName().equals(object.getClass().getCanonicalName())) {
+                Query query = new Query(conn, entity);
+                try {
+                    query.delete(object);
+                    cache.remove(getIdValue(object.getClass(), object));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    private String getIdValue(Class<?> clazz, Object obj) {
+        for (Field field: clazz.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Id.class)) {
+                try {
+                    field.setAccessible(true);
+                    return field.get(obj).toString();
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "0";
+    }
 
-	public Transaction beginTransaction() {
-		if (currenTransaction == null) {
-			currenTransaction = new Transaction(conn);
-			currenTransaction.setAutoCommit(false);
-		}
-		return currenTransaction;
-	}
+    public Query getAll(Class<?> resultType) {
+        for (EntityModel<?> entity : entityModelList) {
+            if (entity.getClassName().equals(resultType.getName())) {
+                Query query = new Query(conn, entity);
+                try {
+                    return query.getAll(resultType);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
 
-	public void close() {
-		try {
-			conn.close();
-			cache.clear();
-			currenTransaction = null;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    public Transaction beginTransaction() {
+        if (currenTransaction == null) {
+            currenTransaction = new Transaction(conn);
+            currenTransaction.setAutoCommit(false);
+        }
+        return currenTransaction;
+    }
+
+    public void close() {
+        try {
+            conn.close();
+            cache.clear();
+            currenTransaction = null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/revanate/session/SessionCache.java
+++ src/main/java/com/revanate/session/SessionCache.java
@@ -1,54 +1,37 @@
 package com.revanate.session;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class SessionCache {
-	
-	// By "Session-based caching" think of a hashmap or some data struture that you would store previously retrieved objects in.
-	// Evey time you retrieve or add an object to an from the DB, you store it in this data structure in your Java program.
-	// That way, when you try to query it a second time, you check the data structure first (your hashmap, for example) and 
-	// if the object exists you can quickly retrieve it without spending time connecting to the DB and executing the same query.
+    
+    private HashMap<String, Object> cache;
+    
+    public SessionCache() {
+        cache = new HashMap<>();
+    }
+    
+    public HashMap<String, Object> get() {
+        return cache;
+    }
 
-	private HashMap<Class<?>, HashSet<Object>> cache;
-	
-	public SessionCache() {
-		cache = new HashMap<>();
-	}
-	
-	public HashMap<Class<?>, HashSet<Object>> get() {
-		return cache;
-	}
-	
-	public HashSet<Object> getObjects(final Class<?> clazz) {
-		return cache.get(clazz);
-	}
-	
-	public void add(final Class<?> clazz, final Object obj) {
-		if (cache.containsKey(clazz)) {
-			cache.get(clazz).add(obj);
-		} else {
-			HashSet<Object> objSet = new HashSet<>();
-			objSet.add(obj);
-			cache.put(clazz, objSet);
-		}
-	}
-	
-	public Object get(final Class<?> clazz, final Object obj) {
-		if (cache.containsKey(clazz)) {
-			HashSet<Object> objSet = cache.get(clazz);
-			for (Object o : objSet) {
-				if (o.equals(obj)) {
-					return o;
-				}
-			}
-			return null;
-		} else {
-			return null;
-		}
-	}
-	
-	public void clear() {
-		cache.clear();
-	}
+    public void add(String key, Object obj) {
+        cache.put(key, obj);
+    }
+    
+    public void remove(String key) {
+        cache.remove(key);
+    }
+    
+    public Object get(String key) {
+        System.out.println("got from cache " + key);
+        return cache.get(key);
+    }
+    
+    public boolean contains(final String key) {
+        return cache.containsKey(key);
+    }
+    
+    public void clear() {
+        cache.clear();
+    }
 }
