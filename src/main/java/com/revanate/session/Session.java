5b7705bdab1b52f9bb735c00dcbd5bd4cda0ef92
Yuriy Koziy <yukoziy@gmail.com>
2022-07-15 08:56:28 -0400

    fixed sesion caching

--- src/main/java/com/revanate/session/Session.java
+++ src/main/java/com/revanate/session/Session.java
@@ -14,135 +14,135 @@ import com.revanate.transaction.Transaction;
 // each session is a connection to the database
 public class Session {
 
-    private Connection conn;
+	private Connection conn;
 
-    private SessionCache cache;
+	private SessionCache cache;
 
-    private Transaction currenTransaction;
+	private Transaction currenTransaction;
 
-    private List<EntityModel<Class<?>>> entityModelList;
+	private List<EntityModel<Class<?>>> entityModelList;
 
-    public Session(Connection conn, List<EntityModel<Class<?>>> entityModelList) {
-        this.entityModelList = entityModelList;
-        this.conn = conn;
-       // cache = new SessionCache();
-        createTables();
-    }
+	public Session(Connection conn, List<EntityModel<Class<?>>> entityModelList) {
+		this.entityModelList = entityModelList;
+		this.conn = conn;
+		cache = new SessionCache();
+		createTables();
+	}
 
-    private void createTables() {
-        Table t = new Table(conn, entityModelList);
-        for (EntityModel<?> entity : entityModelList) {
-            t.createTable(entity);
-        }
-    }
+	private void createTables() {
+		Table t = new Table(conn, entityModelList);
+		for (EntityModel<?> entity : entityModelList) {
+			t.createTable(entity);
+		}
+	}
 
-    public List<EntityModel<Class<?>>> getEntityList() {
-        return entityModelList;
-    }
+	public List<EntityModel<Class<?>>> getEntityList() {
+		return entityModelList;
+	}
 
-    public <T> Object get(Class<?> entityClass, T id) {
-        for (EntityModel<?> entity : entityModelList) {
-            if (entity.getClassName().equals(entityClass.getCanonicalName())) {
-                Query query = new Query(conn, entity);
-                try {
-                    Object resultObject = query.get(entityClass, id);
-                  //  if (cache.contains(id.toString())) {
-                   //     return cache.get(id.toString());
-                  //  } else {
-                  //      cache.add(id.toString(), resultObject);
-                   // }
-                    return resultObject;
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return null;
-    }
+	public <T> Object get(Class<?> entityClass, T id) {
+		for (EntityModel<?> entity : entityModelList) {
+			if (entity.getClassName().equals(entityClass.getCanonicalName())) {
+				Query query = new Query(conn, entity);
+				try {
+					Object resultObject = query.get(entityClass, id);
+					if (cache.contains(id.toString())) {
+						return cache.get(id.toString()).getClass().cast(resultObject);
+					} else {
+						cache.add(id.toString(), resultObject);
+					}
+					return resultObject;
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
 
-    public void update(Object object) {
-        Class<?> clazz = object.getClass();
-        for (EntityModel<?> entity : entityModelList) {
-            if (entity.getClassName().equals(clazz.getCanonicalName())) {
-                Query query = new Query(conn, entity);
-                query.update(object);
-            }
-        }
-    }
+	public void update(Object object) {
+		Class<?> clazz = object.getClass();
+		for (EntityModel<?> entity : entityModelList) {
+			if (entity.getClassName().equals(clazz.getCanonicalName())) {
+				Query query = new Query(conn, entity);
+				query.update(object);
+			}
+		}
+	}
 
-    public Object save(Object object) {
-        Class<?> clazz = object.getClass();
-        for (EntityModel<?> entity : entityModelList) {
-            if (entity.getClassName().equals(clazz.getCanonicalName())) {
-                Query query = new Query(conn, entity);
-                Object resultObject = query.save(object);
-             //   if (resultObject != null) {
-              //      cache.add(resultObject.toString(), object);
-               // }
+	public Object save(Object object) {
+		Class<?> clazz = object.getClass();
+		for (EntityModel<?> entity : entityModelList) {
+			if (entity.getClassName().equals(clazz.getCanonicalName())) {
+				Query query = new Query(conn, entity);
+				Object resultObject = query.save(object);
+				if (resultObject != null) {
+					cache.add(resultObject.toString(), object);
+				}
 
-                return resultObject;
-            }
-        }
-        return null;
-    }
+				return resultObject;
+			}
+		}
+		return null;
+	}
 
-    public void delete(Object object) {
-        for (EntityModel<?> entity : entityModelList) {
-            if (entity.getClassName().equals(object.getClass().getCanonicalName())) {
-                Query query = new Query(conn, entity);
-                try {
-                    query.delete(object);
-                    cache.remove(getIdValue(object.getClass(), object));
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    
-    private String getIdValue(Class<?> clazz, Object obj) {
-        for (Field field: clazz.getDeclaredFields()) {
-            if (field.isAnnotationPresent(Id.class)) {
-                try {
-                    field.setAccessible(true);
-                    return field.get(obj).toString();
-                } catch (IllegalArgumentException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return "0";
-    }
+	public void delete(Object object) {
+		for (EntityModel<?> entity : entityModelList) {
+			if (entity.getClassName().equals(object.getClass().getCanonicalName())) {
+				Query query = new Query(conn, entity);
+				try {
+					query.delete(object);
+					cache.remove(getIdValue(object.getClass(), object));
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    public Query getAll(Class<?> resultType) {
-        for (EntityModel<?> entity : entityModelList) {
-            if (entity.getClassName().equals(resultType.getName())) {
-                Query query = new Query(conn, entity);
-                try {
-                    return query.getAll(resultType);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return null;
-    }
+	private String getIdValue(Class<?> clazz, Object obj) {
+		for (Field field : clazz.getDeclaredFields()) {
+			if (field.isAnnotationPresent(Id.class)) {
+				try {
+					field.setAccessible(true);
+					return field.get(obj).toString();
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return "0";
+	}
 
-    public Transaction beginTransaction() {
-        if (currenTransaction == null) {
-            currenTransaction = new Transaction(conn);
-            currenTransaction.setAutoCommit(false);
-        }
-        return currenTransaction;
-    }
+	public Query getAll(Class<?> resultType) {
+		for (EntityModel<?> entity : entityModelList) {
+			if (entity.getClassName().equals(resultType.getName())) {
+				Query query = new Query(conn, entity);
+				try {
+					return query.getAll(resultType);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
 
-    public void close() {
-        try {
-            conn.close();
-            cache.clear();
-            currenTransaction = null;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	public Transaction beginTransaction() {
+		if (currenTransaction == null) {
+			currenTransaction = new Transaction(conn);
+			currenTransaction.setAutoCommit(false);
+		}
+		return currenTransaction;
+	}
+
+	public void close() {
+		try {
+			conn.close();
+			cache.clear();
+			currenTransaction = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
